e0592d44f3cad861ecda5c6416f124df35886cdc
hyeonjerry <kwonhyeonjae01@gmail.com>
2024-03-19 10:52:13 +0900

    feat: BaseDatetimeEntity 구현

---
+++ server/src/main/java/com/tonggn/techub/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package com.tonggn.techub.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+}
