7396114454350457a5067de899bce3d2cbce200e
hyeonjerry <kwonhyeonjae01@gmail.com>
2024-03-22 12:16:52 +0900

    feat: 외부에서 호출 가능한 RSS 파서 구현

---
+++ server/src/main/java/com/tonggn/techub/crawler/parser/Parser.java
@@ -0,0 +1,16 @@
+package com.tonggn.techub.crawler.parser;
+
+import java.util.List;
+import org.jsoup.nodes.Document;
+
+public class Parser {
+
+  private Parser() {
+  }
+
+  public static List<ParsedFeed> parseRss(final Document document) {
+    final RssType type = RssType.from(document);
+    final RssParser parser = type.getParser();
+    return parser.parse(document);
+  }
+}
