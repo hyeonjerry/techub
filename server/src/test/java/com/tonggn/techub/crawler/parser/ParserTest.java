4738f0e3a9dc065874a36b2278823e23b46c13b7
hyeonjerry <kwonhyeonjae01@gmail.com>
2024-03-25 20:10:00 +0900

    refactor: 파싱 테스트 리팩터링

--- server/src/test/java/com/tonggn/techub/crawler/parser/ParserTest.java
+++ server/src/test/java/com/tonggn/techub/crawler/parser/ParserTest.java
@@ -10,71 +10,18 @@ import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ParserTest {
 
-  private static final String rdfPathname = "src/test/resources/rss/rdf.xml";
-  private static final String rssPathname = "src/test/resources/rss/rss.xml";
-  private static final String atomPathname = "src/test/resources/rss/atom.xml";
-  private final List<ParsedFeed> expected = List.of(
-      new ParsedFeed.Builder("item1", "https://example.com/item/1").build(),
-      new ParsedFeed.Builder("item2", "https://example.com/item/2").build()
-  );
-
-  @Test
-  @DisplayName("Rss 2.0 파싱 테스트")
-  void RssParseTest() throws IOException {
-    // given
-    final File file = new File(rssPathname);
-    final Document document = Jsoup.parse(file, "UTF-8", "", xmlParser());
-
-    // when
-    final List<ParsedFeed> actual = Parser.parseRss(document);
-
-    // then
-    assertThat(actual)
-        .usingRecursiveComparison()
-        .isEqualTo(expected);
-  }
-
-  @Test
-  @DisplayName("RDF 파싱 테스트")
-  void RdfParseTest() throws IOException {
-    // given
-    final File file = new File(rdfPathname);
-    final Document document = Jsoup.parse(file, "UTF-8", "", xmlParser());
-
-    // when
-    final List<ParsedFeed> actual = Parser.parseRss(document);
-
-    // then
-    assertThat(actual)
-        .usingRecursiveComparison()
-        .isEqualTo(expected);
-  }
-
-  @Test
-  @DisplayName("Atom 파싱 테스트")
-  void AtomParseTest() throws IOException {
-    // given
-    final File file = new File(atomPathname);
-    final Document document = Jsoup.parse(file, "UTF-8", "", xmlParser());
-
-    // when
-    final List<ParsedFeed> actual = Parser.parseRss(document);
-
-    // then
-    assertThat(actual)
-        .usingRecursiveComparison()
-        .isEqualTo(expected);
-  }
+  private static final String DOCUMENT_PATH = "src/test/resources/documents/";
 
   @Test
   @DisplayName("Opengraph 파싱 테스트")
   void OpengraphParseTest() throws IOException {
     // given
-    final String pathname = "src/test/resources/opengraph.html";
+    final String pathname = DOCUMENT_PATH + "opengraph.html";
     final File file = new File(pathname);
     final Document document = Jsoup.parse(file);
 
@@ -90,5 +37,65 @@ class ParserTest {
     assertThat(actual)
         .usingRecursiveComparison()
         .isEqualTo(expected);
+  }
+
+  @Nested
+  class RssTest {
+
+    private final List<ParsedFeed> expected = List.of(
+        new ParsedFeed.Builder("item1", "https://example.com/item/1").build(),
+        new ParsedFeed.Builder("item2", "https://example.com/item/2").build()
+    );
+
+    @Test
+    @DisplayName("Rss 2.0 파싱 테스트")
+    void RssParseTest() throws IOException {
+      // given
+      final String pathname = DOCUMENT_PATH + "rss.xml";
+      final File file = new File(pathname);
+      final Document document = Jsoup.parse(file, "UTF-8", "", xmlParser());
+
+      // when
+      final List<ParsedFeed> actual = Parser.parseRss(document);
+
+      // then
+      assertThat(actual)
+          .usingRecursiveComparison()
+          .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("RDF 파싱 테스트")
+    void RdfParseTest() throws IOException {
+      // given
+      final String pathname = DOCUMENT_PATH + "rdf.xml";
+      final File file = new File(pathname);
+      final Document document = Jsoup.parse(file, "UTF-8", "", xmlParser());
+
+      // when
+      final List<ParsedFeed> actual = Parser.parseRss(document);
+
+      // then
+      assertThat(actual)
+          .usingRecursiveComparison()
+          .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Atom 파싱 테스트")
+    void AtomParseTest() throws IOException {
+      // given
+      final String pathname = DOCUMENT_PATH + "atom.xml";
+      final File file = new File(pathname);
+      final Document document = Jsoup.parse(file, "UTF-8", "", xmlParser());
+
+      // when
+      final List<ParsedFeed> actual = Parser.parseRss(document);
+
+      // then
+      assertThat(actual)
+          .usingRecursiveComparison()
+          .isEqualTo(expected);
+    }
   }
 }
