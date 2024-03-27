76b1192d8f8c2a11e8516cab32caf5ebcb42ab60
hyeonjerry <kwonhyeonjae01@gmail.com>
2024-03-27 17:07:01 +0900

    feat: url 디코딩 및 텍스트 길이 제한 기능 구현

--- server/src/main/java/com/tonggn/techub/domain/Feed.java
+++ server/src/main/java/com/tonggn/techub/domain/Feed.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Feed {
 
+  private static final int DESC_LIMIT_LENGTH = 255;
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -35,12 +38,31 @@ public class Feed {
 
   private String thumbnailUrl;
 
-  public Feed(final Publisher publisher, final String title, final String url, final String description,
-      final String thumbnailUrl) {
+  public Feed(
+      final Publisher publisher,
+      final String title,
+      final String url,
+      final String description,
+      final String thumbnailUrl
+  ) {
     this.publisher = publisher;
     this.title = title;
-    this.url = url;
-    this.description = description;
-    this.thumbnailUrl = thumbnailUrl;
+    this.url = decodeUrl(url);
+    this.description = trimDescription(description);
+    this.thumbnailUrl = decodeUrl(thumbnailUrl);
   }
-}
+
+  private static String decodeUrl(final String url) {
+    if (url == null) {
+      return null;
+    }
+    return URLDecoder.decode(url, StandardCharsets.UTF_8);
+  }
+
+  private static String trimDescription(final String desc) {
+    if (desc == null) {
+      return null;
+    }
+    return desc.length() > DESC_LIMIT_LENGTH ? desc.substring(0, DESC_LIMIT_LENGTH) : desc;
+  }
+  }
